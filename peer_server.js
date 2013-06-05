e0674c6a7daebce1252ebf37ed96c8a8e751a39b
Charles Holbrow <charlesholbrow@gmail.com>
2013-06-04 17:33:50 -0700

    Handlebars for for rendering html on the server

--- peer_server.js
+++ peer_server.js
@@ -3,17 +3,24 @@ var Handlebars  = require('handlebars');
 var fs          = require('fs');
 
 var exServe = require('express')();
-var template = fs.readFileSync('public/index.html', {encoding: 'utf8'});
+var source = fs.readFileSync('public/index.html', {encoding: 'utf8'});
+var template = Handlebars.compile(source)
 
 exServe.get('/', function(req, res){
   res.send('Try this - /:name/to/:target');
 });
 
 exServe.get('/:name/to/:target', function(req, res){
-  res.send(template);
+  var pathDirs = req.url.split('/');
+  var context = {
+    name: pathDirs[1],
+    target: pathDirs[3]
+  };
+
+  res.send(template(context));
 });
 
-console.log("launching express server on 9001")
+console.log("launching express server on 9001");
 exServe.listen("9001");
 
 console.log("launch peer server on 9000");
